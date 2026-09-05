feba75e7d97e72bd0d091c1a1515db63fbdeddc9
H82M19\Student <Student>
2026-09-05 18:58:22 +0700

    Cau 3: Reject Promise with error

--- PromiseBasics.ts
+++ PromiseBasics.ts
@@ -21,3 +21,16 @@ function getNumber(): Promise<number> {
 getNumber().then((result) => {
     console.log("Câu 2:", result);
 });
+
+// Câu 3
+function getError(): Promise<string> {
+    return new Promise((_, reject) => {
+        setTimeout(() => {
+            reject(new Error("Something went wrong"));
+        }, 1000);
+    });
+}
+
+getError().catch((error) => {
+    console.log("Câu 3:", error.message);
+});
