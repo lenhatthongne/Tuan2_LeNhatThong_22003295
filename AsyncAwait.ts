25cdbb8d1eb030a26f0fea87c08457c61d43499a
H82M19\Student <Student>
2026-09-05 19:12:27 +0700

    Cau 13: Handle error with try catch

--- AsyncAwait.ts
+++ AsyncAwait.ts
@@ -13,3 +13,38 @@ async function runCau11() {
 }
 
 runCau11();
+
+// Câu 12
+function simulateTask(time: number): Promise<string> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Task done");
+        }, time);
+    });
+}
+
+async function runCau12() {
+    const result = await simulateTask(2000);
+    console.log("Câu 12:", result);
+}
+
+runCau12();
+
+// Câu 13
+function createError(): Promise<never> {
+    return new Promise((_, reject) => {
+        setTimeout(() => {
+            reject(new Error("Something went wrong"));
+        }, 1000);
+    });
+}
+
+async function runCau13() {
+    try {
+        await createError();
+    } catch (error) {
+        console.log("Câu 13:", (error as Error).message);
+    }
+}
+
+runCau13();
